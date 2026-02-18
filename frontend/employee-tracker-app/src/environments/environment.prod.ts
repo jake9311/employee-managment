dc7fd45b42143340a51f054ab761e64606e7d2cb
Yakov <ymakoria@gmail.com>
2026-02-18 14:28:47 +0200

    remove Angular environment paths from .gitignore and add production environment configuration

---
+++ frontend/employee-tracker-app/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCiptuXzY_8jLQg2KSjIh9KbnlU9lz7rPo",
+    authDomain: "employeetracker-a4046.firebaseapp.com",
+    projectId: "employeetracker-a4046",
+    storageBucket: "employeetracker-a4046.firebasestorage.app",
+    messagingSenderId: "232784068059",
+    appId: "1:232784068059:web:50cc463bb7dea8b941fbfe",
+    measurementId: "G-E3VKF21EC3"
+  },
+  apiUrl: "https://employee-managment-kmgu.onrender.com"
+};
